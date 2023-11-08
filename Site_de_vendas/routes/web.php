5e3a2f39d1d9df69d004e497b9f10b33493665da
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-11-08 14:54:47 -0300

    Controllers

--- Site_de_vendas/routes/web.php
+++ Site_de_vendas/routes/web.php
@@ -1,38 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return redirect()->route('admin.clientes');
-});
+use App\Http\Controllers\ProdutoController;
 
 
-Route::group([
-    'prefix' => 'admin',
-    'as' => 'admin.'
-], function(){
 
-    Route::get('dashboard', function(){
-        return "dashboard";
-    })->name('dashboard');
-    
-    Route::get('users', function(){
-        return "users";
-    })->name('users');
-    
-    Route::get('clientes', function(){
-        return "clientes";
-    })->name('clientes');
+Route::get('/', [ProdutoController::class, 'index'] );
 
-});
