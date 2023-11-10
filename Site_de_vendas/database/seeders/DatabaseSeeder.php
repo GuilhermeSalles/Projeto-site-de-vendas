bb9631fdb199fd0dd2898bc3ba1e069516ddaafb
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-11-10 12:34:48 -0300

    Factory

--- Site_de_vendas/database/seeders/DatabaseSeeder.php
+++ Site_de_vendas/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Categoria;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UsersSeeder::class,
+            CategoriasSeeder::class,
         ]);
     }
 }
