5cb930e5095d85ca7f8fdb731d8233fc422abc04
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-11-14 10:56:25 -0300

    OneToMany Inverse / belongsTo

--- Site_de_vendas/resources/views/site/details.blade.php
+++ Site_de_vendas/resources/views/site/details.blade.php
@@ -13,6 +13,9 @@
         <div class="col s12 m6">
             <h1>{{ $produto->nome }}</h1>
             <p> {{ $produto->descricao }} </p>
+            <p>Postado por: {{ $produto->user->firstName }} </p>
+            <p>Categoria: {{ $produto->categoria->nome }} </p>
+
 
             <button class="btn orange btn-large">Comprar</button>
         </div>
