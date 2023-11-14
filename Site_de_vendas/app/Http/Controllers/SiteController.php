023ddc72b6ddaad96e74af5dc03b55842345f8db
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-11-14 11:41:29 -0300

    Mostrar nome da categoria e preço numberFormat

--- Site_de_vendas/app/Http/Controllers/SiteController.php
+++ Site_de_vendas/app/Http/Controllers/SiteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \App\Models\Produto;
+use App\Models\Categoria;
 
 class SiteController extends Controller
 {
@@ -27,8 +28,9 @@ class SiteController extends Controller
 
     public function categoria($id)
     {
+        $categoria = Categoria::find('id');
         $produtos = Produto::where('id_categoria', $id)->paginate(3);
 
-        return view('site.categoria', compact('produtos'));
+        return view('site.categoria', compact('produtos','categoria'));
     }
 }
