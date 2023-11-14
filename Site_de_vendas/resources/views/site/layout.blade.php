d109180b512902c150527df6d45787ddb1a5d7d4
GuilhermeSalles <guibaltazarvs@gmail.com>
2023-11-14 11:29:38 -0300

    Categorias dinâmicas com viewShare

--- Site_de_vendas/resources/views/site/layout.blade.php
+++ Site_de_vendas/resources/views/site/layout.blade.php
@@ -14,21 +14,41 @@
 
 <body>
 
+
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+        @foreach ($categoriasMenu as $categoriaM)
+            <li><a href="#!"> {{ $categoriaM->nome }} </a></li>
+        @endforeach
+    </ul>
+
+
     <nav class="red">
         <div class="nav-wrapper container ">
-          <a href="#" class="brand-logo center">Curso Laravel</a>
-          <ul id="nav-mobile" class="left">
-            <li><a href="">Home</a></li>
-            <li><a href="">Carrinho</a></li>
-          </ul>
+            <a href="#" class="brand-logo center">Curso Laravel</a>
+            <ul id="nav-mobile" class="left">
+                <li><a href="">Home</a></li>
+                <li><a href="" class="dropdown-trigger" data-target="dropdown1">Categorias <i
+                            class="material-icons right">expand_more</i> </a></li>
+                <li><a href="">Carrinho</a></li>
+            </ul>
         </div>
-      </nav>
-            
+    </nav>
+
 
     @yield('conteudo')
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+        /* Dropdown */
+        var elemDrop = document.querySelectorAll('.dropdown-trigger');
+        var instanceDrop = M.Dropdown.init(elemDrop, {
+            coverTrigger: false,
+            constrainWidth: false
+        });
+    </script>
 </body>
 
 </html>
